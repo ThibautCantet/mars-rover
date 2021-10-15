d0431da4399d04e6d48cfc6ee06f650f3a2d38ca
Thibaut Cantet <thibaut.cantet@soat.fr>
2021-10-15 13:34:45 +0200

    test initiale coordonnees

--- src/main/java/com/mars/Rover.java
+++ src/main/java/com/mars/Rover.java
@@ -1,17 +1,29 @@
 package com.mars;
 
 public class Rover {
-    private Direction direction;
+    private final int x;
+    private final int y;
+    private final Direction direction;
 
-    private Rover(Direction direction) {
+    private Rover(int x, int y, Direction direction) {
+        this.x = x;
+        this.y = y;
         this.direction = direction;
     }
 
     public static Rover create(int x, int y, Direction direction) {
-        return new Rover(direction);
+        return new Rover(x, y, direction);
     }
 
     public Direction getDirection() {
         return direction;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public Integer getY() {
+        return y;
     }
 }
--- src/test/java/com/mars/RoverTest.java
+++ src/test/java/com/mars/RoverTest.java
@@ -1,8 +1,10 @@
 package com.mars;
 
+import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
@@ -38,6 +40,22 @@ class RoverTest {
         private static Stream<Direction> provideDirections() {
             return Stream.of(Direction.values());
         }
+
+        @ParameterizedTest
+        @MethodSource("provideCoordonnees")
+        void le_rover_est_positionne_aux_coordonnees_initiales(Integer x, Integer y) {
+            // when, act
+            final Rover rover = Rover.create(x, y, Direction.W);
+
+            // then, assert
+            assertThat(rover.getX()).isEqualTo(x);
+            assertThat(rover.getY()).isEqualTo(y);
+        }
+
+        private static Stream<Arguments> provideCoordonnees() {
+            return Stream.of(Arguments.of(1, 2), Arguments.of(2, 3));
+        }
+
     }
 }
 
